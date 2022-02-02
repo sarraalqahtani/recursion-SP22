0813f805d9cc0e797d4b959561022f89352ded01
sarraalqahtani <53792971+sarraalqahtani@users.noreply.github.com>
2022-02-02 17:26:24 -0500

    Initial commit

---
+++ src/Recursion.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Recursion {
+
+    public static int Exponent(int x , int m){
+        //fix me
+
+    }
+
+    public static void main (String [] args){
+        Scanner reader= new Scanner(System.in);
+        System.out.println("Enter x");
+        int x= reader.nextInt();
+        System.out.println("Enter m");
+        int m= reader.nextInt();
+        //call the recursive function Exponent and print the results to the screen
+    }
+}
